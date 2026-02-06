e2059ddf5461184940e4cd75a536f53722e108fa
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2026-02-05 18:29:50 -0800

    simplify examples

--- examples/mini_sy04/cancel_action.py
+++ examples/mini_sy04/cancel_action.py
@@ -7,12 +7,7 @@ from random import uniform
 
 from runze_control.syringe_pump import MiniSY04
 
-# Uncomment for some prolific log statements.
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
-logger.handlers[-1].setFormatter(
-    logging.Formatter(fmt='%(asctime)s:%(name)s:%(levelname)s: %(message)s'))
+logging.basicConfig(level=logging.DEBUG)
 
 
 # Constants
@@ -21,16 +16,16 @@ COM_PORT = "/dev/ttyUSB0"
 # Connect to a single pump.
 syringe_pump = MiniSY04(COM_PORT, address=0x00, syringe_volume_ul=20000)
 
-logger.info(f"Syringe address: {syringe_pump.get_address()}")
-logger.info(f"Syringe baud rate: {syringe_pump.get_rs232_baudrate()}")
-logger.info("Resetting syringe.")
+logging.info(f"Syringe address: {syringe_pump.get_address()}")
+logging.info(f"Syringe baud rate: {syringe_pump.get_rs232_baudrate()}")
+logging.info("Resetting syringe.")
 syringe_pump.reset_syringe_position()
-logger.info(f"Moving plunger (in percent.)")
+logging.info(f"Moving plunger (in percent.)")
 syringe_pump.move_absolute_in_percent(0) # wait = True
 syringe_pump.set_speed_percent(20)
 for i in range(10):
     sleep(0.5)
-    logger.info(f"Starting 50% full-scale range move.")
+    logging.info(f"Starting 50% full-scale range move.")
     syringe_pump.move_absolute_in_percent(10, wait=False)
     print()
     sleep(uniform(0, 1)*0.25)
@@ -38,8 +33,8 @@ for i in range(10):
         syringe_pump.halt()
         sleep(0.05)
     print()
-    logger.info(f"Resuming movement.")
+    logging.info(f"Resuming movement.")
     syringe_pump.move_absolute_in_percent(0)
-    logger.info(f"Syringe position: {syringe_pump.get_position_percent(): .3f}%")
+    logging.info(f"Syringe position: {syringe_pump.get_position_percent(): .3f}%")
     print()
     sleep(1)
